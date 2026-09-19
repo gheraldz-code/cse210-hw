fcd60e0c3667f7ff854a72f6a10530d9efaff675
gheraldz-code <gheraldz@gmail.com>
2026-09-19 07:13:30 -0400

    upload fractions to github

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction firstFraction = new Fraction();
+        Console.WriteLine(firstFraction.GetFractionString());
+        Console.WriteLine(firstFraction.GetDecimalValue());
+
+        Fraction secondFraction = new Fraction(5);
+        Console.WriteLine(secondFraction.GetFractionString());
+        Console.WriteLine(secondFraction.GetDecimalValue());
+
+        Fraction thirdFraction = new Fraction(3,4);
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.WriteLine(thirdFraction.GetDecimalValue());
+
+        thirdFraction.SetTop(1);
+        thirdFraction.SetBottom(3);
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.WriteLine(thirdFraction.GetDecimalValue());
     }
 }
